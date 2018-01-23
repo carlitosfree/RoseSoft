107a7619b6557238ae2663f28862155730b01d3a
carlitosfree <ronald_96c@hotmail.com>
2018-01-23 16:02:21 -0500

    factura

--- RoseSoft/RoseSoft/Factura.Designer.cs
+++ RoseSoft/RoseSoft/Factura.Designer.cs
@@ -36,12 +36,32 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DescripcionFactura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precioUnitarioFactura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TotalFactura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UnidadesFactura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NumeroCajasFactura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TipoCajaFactura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TallosFactura = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Location = new System.Drawing.Point(2, 2);
             this.panel1.Name = "panel1";
@@ -50,6 +70,10 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.textBox2);
@@ -106,11 +130,123 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 96);
+            this.label3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label3.Location = new System.Drawing.Point(0, 442);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Cédula / RUC";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Dirección";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Teléfono";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Fecha";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.comboBox1);
+            this.panel3.Location = new System.Drawing.Point(295, 8);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(339, 82);
+            this.panel3.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dataGridView1);
+            this.panel4.Location = new System.Drawing.Point(297, 96);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(338, 362);
+            this.panel4.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DescripcionFactura,
+            this.precioUnitarioFactura,
+            this.TotalFactura,
+            this.UnidadesFactura,
+            this.NumeroCajasFactura,
+            this.TipoCajaFactura,
+            this.TallosFactura});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(328, 348);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // DescripcionFactura
+            // 
+            this.DescripcionFactura.HeaderText = "Descripción";
+            this.DescripcionFactura.Name = "DescripcionFactura";
+            // 
+            // precioUnitarioFactura
+            // 
+            this.precioUnitarioFactura.HeaderText = "Precio Unitario";
+            this.precioUnitarioFactura.Name = "precioUnitarioFactura";
+            // 
+            // TotalFactura
+            // 
+            this.TotalFactura.HeaderText = "Total";
+            this.TotalFactura.Name = "TotalFactura";
+            // 
+            // UnidadesFactura
+            // 
+            this.UnidadesFactura.HeaderText = "Unidades";
+            this.UnidadesFactura.Name = "UnidadesFactura";
+            // 
+            // NumeroCajasFactura
+            // 
+            this.NumeroCajasFactura.HeaderText = "# Cajas";
+            this.NumeroCajasFactura.Name = "NumeroCajasFactura";
+            // 
+            // TipoCajaFactura
+            // 
+            this.TipoCajaFactura.HeaderText = "Tipo Caja";
+            this.TipoCajaFactura.Name = "TipoCajaFactura";
+            // 
+            // TallosFactura
+            // 
+            this.TallosFactura.HeaderText = "# Tallos";
+            this.TallosFactura.Name = "TallosFactura";
             // 
             // Factura
             // 
@@ -123,6 +259,9 @@
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -137,5 +276,20 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DescripcionFactura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precioUnitarioFactura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TotalFactura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UnidadesFactura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NumeroCajasFactura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipoCajaFactura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TallosFactura;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
     }
 }
